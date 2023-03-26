e6613f136676576a0631ee71ac741d1dcf0bde34
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2023-03-26 12:33:15 +0100

    Attempting to fix this `Damning CORS error`

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ const { default: mongoose } = require("mongoose");
 const Student = require("./Models/Students");
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: "*",
   })
 );
-app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(require("./requirements"));
 
 app.get("/", (req, res) => {
   res.json({
     server: true,
-    database: false,
   });
 });
 
